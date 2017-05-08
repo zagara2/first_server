75ee540092e6b79df1203f0796d20b3f0329c838
Mickey S <marimikaels@gmail.com>
2017-05-08 19:25:50 -0400

    corrected

--- myfirstserver.js
+++ myfirstserver.js
@@ -7,22 +7,32 @@ var PORT = 7000;
 var port2 = 7500;
 
 // Here we create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest1(request, response) {
 
   // The below statement is triggered (client-side) when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("you're cool I suppose");
+  
 }
+
+function handleRequest2(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("i don't like ya");
+  
+}
+
 
 // Here we use the Node HTTP package to create our server.
 // We then pass it the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
 
 // Here we start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
 
   // The below statement is triggered (server-side) when a user visits the PORT URL
   //%s just means "substitute here what you put after comma" or something
-  console.log("you're cool I suppose");
+  
   console.log("Server listening on: http://localhost:%s", PORT);
 
 });
@@ -31,7 +41,7 @@ server.listen(port2, function() {
 
   // The below statement is triggered (server-side) when a user visits the PORT2 URL
   //%s just means "substitute here what you put after comma" or something
-  console.log("i don't like ya");
+  
   console.log("Server listening on: http://localhost:%s", port2);
 
 });
